d51fadeadb8922beb3206197de40523995c6fee0
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-07-13 16:34:12 +0530

    some messages

--- backend.sh
+++ backend.sh
@@ -79,7 +79,7 @@ VALIDATE $? "Enabling backend"
 dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing MySQL Client"
 
-mysql -h 172.31.94.66 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+mysql -h 35.174.138.101 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
 VALIDATE $? "MYSQL Schema loading"
 
 systemctl restart backend &>>$LOGFILE
